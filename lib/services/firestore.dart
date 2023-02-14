26fce312f2a0a92fda3c1915de0454490e9760b6
Anand <anandanairaan@gmail.com>
2023-02-14 12:37:37 +0530

    update

--- lib/services/firestore.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class Firestore {
-  final db = FirebaseFirestore.instance;
-
-  Future<void> createUserInFirestore(user) async {
-    print(user);
-  }
-}
